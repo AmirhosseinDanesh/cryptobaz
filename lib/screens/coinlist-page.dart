48e5e74eca3cfa495b137aa60ddad4d5b0d91c37
Amir Danesh <62671734+AmirhosseinDanesh@users.noreply.github.com>
2023-12-18 21:16:48 +0330

    build list structer for showing price and info

--- lib/screens/coinlist-page.dart
+++ lib/screens/coinlist-page.dart
@@ -24,22 +24,57 @@ class _CoinListScreenState extends State<CoinListScreen> {
         child: ListView.builder(
           itemCount: cryptoList!.length,
           itemBuilder: (context, index) {
-            return Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Container(
-                height: 150,
-                color: Colors.green,
-                child: Center(
-                  child: Text(
-                    cryptoList![index].name,
-                    style: TextStyle(color: Colors.white),
+            return ListTile(
+                title: Text(cryptoList![index].name),
+                subtitle: Text(cryptoList![index].symbol),
+                leading: SizedBox(
+                  width: 30.0,
+                  child: Center(
+                    child: Text(cryptoList![index].rank.toString()),
                   ),
                 ),
-              ),
-            );
+                trailing: SizedBox(
+                  width: 150,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            cryptoList![index].priceUsd.toStringAsFixed(2),
+                          ),
+                          Text(cryptoList![index]
+                              .changePercent24Hr
+                              .toStringAsFixed(2)),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 30.0,
+                        child: Center(
+                            child: _getIconForChangePercent(
+                                cryptoList![index].changePercent24Hr)),
+                      )
+                    ],
+                  ),
+                ));
           },
         ),
       ),
     );
+  }
+
+  Widget _getIconForChangePercent(double changePercent) {
+    return changePercent <= 0
+        ? Icon(
+            Icons.trending_down,
+            size: 24,
+            color: Colors.red,
+          )
+        : Icon(
+            Icons.trending_up,
+            size: 24,
+            color: Colors.green,
+          );
   }
 }
